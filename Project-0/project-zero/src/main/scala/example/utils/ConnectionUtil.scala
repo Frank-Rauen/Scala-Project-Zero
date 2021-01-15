3ed06490204d1eec19f8441c50c258d063fe8bbc
Frank Rauen <home@Franks-MacBook-Air.local>
2021-01-15 12:06:08 -0700

    adds utils

---
+++ Project-0/project-zero/src/main/scala/example/utils/ConnectionUtil.scala
@@ -0,0 +1,34 @@
+package example.utils
+
+import java.sql.Connection
+import java.sql.DriverManager
+
+object ConnectionUtil {
+
+  var conn: Connection = null;
+
+  /** utility for retrieving connection, with hardcorded credentials
+    *
+    * This should properly be a connection pool.  Instead, we'll use it
+    * like a connection pool with a single connection, that gets returned
+    * whenever any part of our application needs it
+    *
+    * @return Connection
+    */
+  def getConnection(): Connection = {
+
+    // if conn is null or closed, initialize it
+    if (conn == null || conn.isClosed()) {
+      classOf[org.postgresql.Driver].newInstance() // manually load the Driver
+
+      //missing a bit of documentation from the java code:
+      // getConnection takes a url, username, password
+      // hardcoding these at the moment, though this is v bad practice
+    DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", 
+                                "home", 
+                                "Wordpass69!")
+
+        }
+        conn
+    }
+}
